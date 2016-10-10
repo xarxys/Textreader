e1ad92f23be5537740746c65aa43d792123ffc5c
Protsenko Vasiliy <Prochenkovas@gmail.com>
2016-10-10 22:16:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vasiliy/textreader/textreader.java
@@ -0,0 +1,13 @@
+package com.example.vasiliy.textreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class textreader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_textreader);
+    }
+}
